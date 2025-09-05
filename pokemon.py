7cc66c1bbfebab841f1354f07c38fd52304c98ef
Callum Bain <cbain@spartaglobal.com>
2025-09-05 16:14:19 +0100

    refactored pokemon.py to fit into functions, added types and abilities tables, added created erd

--- pokemon.py
+++ pokemon.py
@@ -1,58 +1,162 @@
+import pandas as pd
+import requests
+from pprint import pprint
 from sqlalchemy import create_engine, text
 from sqlalchemy_utils import database_exists, create_database
-import pyodbc
-import requests
-import pandas as pd
-from pprint import pprint
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.width', None)
+# Configuration
+def configure_pandas():
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.width', None)
 
-def request_data(url):
+# API Request
+def get_pokemon_data(pokemon_id):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}'
     response = requests.get(url)
-    raw_data = response.json()
-    raw_data.pop('base_experience')
-    raw_data.pop('is_default')
-    raw_data.pop('order')
-    raw_data.pop('forms')
-    raw_data.pop('game_indices')
-    raw_data.pop('held_items')
-    raw_data.pop('location_area_encounters')
-    raw_data.pop('sprites')
-    raw_data.pop('cries')
-    raw_data.pop('past_types')
-    raw_data.pop('past_abilities')
-    raw_data.pop('moves')
-    raw_data.pop('stats')
-    raw_data.pop('types')
-    raw_data.pop('species')
-    raw_data.pop('abilities')
-    return raw_data
+    return response.json()
 
-all_pokemon = []
-for i in range(1, 11):
-    raw_data = request_data(f'https://pokeapi.co/api/v2/pokemon/{i}')
-    all_pokemon.append(raw_data)
+# Fetch Multiple Pokemon (Raw Data)
+def fetch_multiple_pokemon(n=10):
+    all_pokemon_raw = []
+    for i in range(1, n + 1):
+        raw_data = get_pokemon_data(i)
+        all_pokemon_raw.append(raw_data)
+    return all_pokemon_raw
 
-pprint(all_pokemon[0])
-df_pokemon = pd.DataFrame(columns = ['id', 'name', 'height', 'weight'])
-new_rows = pd.DataFrame(all_pokemon)
-df_pokemon = pd.concat([df_pokemon, new_rows], ignore_index=True)
-print(df_pokemon)
+# Clean Raw JSON for Pokemon Table
+def clean_for_pokemon_table(raw_data):
+    return {
+        'id': raw_data['id'],
+        'name': raw_data['name'],
+        'height': raw_data['height'],
+        'weight': raw_data['weight']
+    }
 
-# https://pokeapi.co/api/v2/evolution-chain/14/
+# Clean Raw JSON for Type Tables
+def clean_for_type_table(raw_data):
+    type_set = set()
+    pokemon_types = []
+    for pokemon in raw_data:
+        pokemon_id = pokemon['id']
+        for type in pokemon['types']:
+            type_name = type['type']['name']
+            type_url = type['type']['url']
+            type_id = int(type_url.strip("/").split("/")[-1])
+            slot = type['slot']
+            type_set.add((type_id, type_name))
+            pokemon_types.append({
+                'pokemon_id': pokemon_id,
+                'type_id': type_id,
+                'slot': slot
+            })
+    df_type = pd.DataFrame(type_set, columns=['id', 'name']).sort_values('id')
+    df_pokemon_type = pd.DataFrame(pokemon_types)
+    return df_type, df_pokemon_type
 
-engine = create_engine("mssql+pyodbc://(localdb)\\MSSQLLocalDB/Pokemon?driver=ODBC+Driver+17+for+SQL+Server")
-if not database_exists(engine.url):
-    create_database(engine.url)
+# Clean Raw JSON for Abilities Tables
+def clean_for_abilities_tables(raw_data):
+    ability_set = set()
+    pokemon_abilities = []
+    for pokemon in raw_data:
+        pokemon_id = pokemon['id']
+        for ability in pokemon['abilities']:
+            ability_name = ability['ability']['name']
+            ability_url = ability['ability']['url']
+            ability_id = int(ability_url.strip("/").split("/")[-1])
+            slot = ability['slot']
+            is_hidden = ability['is_hidden']
+            ability_set.add((ability_id, ability_name))
+            pokemon_abilities.append({
+                'pokemon_id': pokemon_id,
+                'ability_id': ability_id,
+                'slot': slot,
+                'is_hidden': is_hidden
+            })
+    df_ability = pd.DataFrame(ability_set, columns=['id', 'name']).sort_values('id')
+    df_pokemon_ability = pd.DataFrame(pokemon_abilities)
+    return df_ability, df_pokemon_ability
 
-df_pokemon.to_sql(name='pokemon', con=engine, if_exists='replace')
+# Create Type Tables
+def create_type_tables(df_type, df_pokemon_type, engine):
+    df_type.to_sql(name='type', con=engine, if_exists='replace', index=False)
+    df_pokemon_type.to_sql(name='pokemon_type', con=engine, if_exists='replace', index=False)
+    print("Types Tables Created")
 
-with engine.connect() as conn:
-    result = conn.execute(text('SELECT * FROM pokemon;'))
-    for row in result:
-        print(row)
+# Create Abilities Tables
+def create_abilities_tables(df_ability, df_pokemon_ability, engine):
+    df_ability.to_sql(name='ability', con=engine, if_exists='replace', index=False)
+    df_pokemon_ability.to_sql(name='pokemon_ability', con=engine, if_exists='replace', index=False)
+
+# Create Pokemon DataFrame
+def create_pokemon_dataframe(pokemon_raw_list):
+    cleaned = [clean_for_pokemon_table(p) for p in pokemon_raw_list]
+    return pd.DataFrame(cleaned)
+
+# Database Setup
+def setup_database_connection():
+    engine = create_engine("mssql+pyodbc://(localdb)\\MSSQLLocalDB/Pokemon?driver=ODBC+Driver+17+for+SQL+Server")
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    return engine
+
+# Save to SQL
+def save_dataframe_to_sql(df, engine, table_name='pokemon'):
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+
+# Query and Print
+def query_and_print_table(engine, table_name='pokemon'):
+    with engine.connect() as conn:
+        result = conn.execute(text(f'SELECT * FROM {table_name};'))
+        for row in result:
+            print(row)
+
+# Main
+def main():
+    configure_pandas()
+
+    # Fetch raw data
+    pokemon_raw_list = fetch_multiple_pokemon(10)
+
+    # pprint(pokemon_raw_list[0])  # For debugging
+
+    # Clean for Pokemon table
+    df_pokemon = create_pokemon_dataframe(pokemon_raw_list)
+
+    # Clean for types and pokemon_type tables
+    df_type, df_pokemon_type = clean_for_type_table(pokemon_raw_list)
+
+    # Clean for ability and pokemon ability table
+    df_ability, df_pokemon_ability = clean_for_abilities_tables(pokemon_raw_list)
+
+    # DB Connection
+    engine = setup_database_connection()
+
+    # Save Tables
+    save_dataframe_to_sql(df_pokemon, engine, table_name='pokemon')
+    create_type_tables(df_type, df_pokemon_type, engine)
+    create_abilities_tables(df_ability, df_pokemon_ability, engine)
+
+    # Query to verify
+    query_and_print_table(engine, table_name='pokemon')
+    query_and_print_table(engine, table_name='type')
+    query_and_print_table(engine, table_name='pokemon_type')
+    query_and_print_table(engine, table_name='ability')
+    query_and_print_table(engine, table_name='pokemon_ability')
+
+if __name__ == "__main__":
+    main()
 
 
 
+'''
+Order:
+type + pokemon_type=DONE
+abilities + pokemon_abilities	
+moves + pokemon_moves	
+items + pokemon_held_items	
+stats	
+evolution_chain / species
+'''
+
+# Add paulaked as collaborator
