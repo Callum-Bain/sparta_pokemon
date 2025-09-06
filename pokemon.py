657c682d35b8fefe72ecf5c29711875e49dfca71
Callum Bain <cbain@spartaglobal.com>
2025-09-06 15:06:18 +0100

    cleaned code slightly, removed pokemon api_id column, added response statements in main

--- pokemon.py
+++ pokemon.py
@@ -4,16 +4,13 @@ from pprint import pprint
 from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, ForeignKey
 from sqlalchemy_utils import database_exists, create_database
 
-# Configuration
-def configure_pandas():
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', None)
 
 # API Request
 def get_pokemon_data(pokemon_id):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}'
     response = requests.get(url)
     return response.json()
+
 
 # Fetch Multiple Pokemon (Raw Data)
 def fetch_multiple_pokemon(n=10):
@@ -23,18 +20,19 @@ def fetch_multiple_pokemon(n=10):
         all_pokemon_raw.append(raw_data)
     return all_pokemon_raw
 
+
 # Clean Raw JSON for Pokemon Table
 def clean_for_pokemon_table(raw_data):
     pokemon_cleaned = []
     for pokemon in raw_data:
         pokemon_cleaned.append({
-            'api_id': pokemon['id'],
             'name': pokemon['name'],
             'height': pokemon['height'],
             'weight': pokemon['weight']
         })
     df_pokemon = pd.DataFrame(pokemon_cleaned)
     return df_pokemon
+
 
 # Clean Raw JSON for Type Tables
 def clean_for_type_table(raw_data):
@@ -53,6 +51,7 @@ def clean_for_type_table(raw_data):
     df_pokemon_type = pd.DataFrame(pokemon_types)
     return df_type, df_pokemon_type
 
+
 # Clean Raw JSON for Abilities Tables
 def clean_for_abilities_tables(raw_data):
     ability_names = []
@@ -69,6 +68,7 @@ def clean_for_abilities_tables(raw_data):
     df_ability = pd.DataFrame(ability_names).drop_duplicates().sort_values('name')
     df_pokemon_ability = pd.DataFrame(pokemon_abilities)
     return df_ability, df_pokemon_ability
+
 
 # Clean Raw JSON for Moves Tables
 def clean_for_moves_tables(raw_data):
@@ -93,86 +93,91 @@ def clean_for_moves_tables(raw_data):
     df_pokemon_move = pd.DataFrame(pokemon_moves)
     return df_move, df_pokemon_move
 
+
 # Create Empty Tables
 def create_empty_tables(engine):
     metadata = MetaData()
 
     pokemon = Table('pokemon', metadata,
         Column('id', Integer, primary_key=True),
-        Column('api_id', Integer, unique=True),
         Column('name', String(100)),
         Column('height', Integer),
         Column('weight', Integer))
 
-    type_table = Table('type', metadata,
+    type_table = Table('types', metadata,
         Column('id', Integer, primary_key=True),
         Column('name', String(100), unique=True))
 
-    pokemon_type = Table('pokemon_type', metadata,
+    pokemon_type = Table('pokemon_types', metadata,
         Column('pokemon_id', Integer, ForeignKey('pokemon.id', ondelete='CASCADE')),
-        Column('type_id', Integer, ForeignKey('type.id', ondelete='CASCADE')))
+        Column('type_id', Integer, ForeignKey('types.id', ondelete='CASCADE')))
 
-    ability = Table('ability', metadata,
+    ability = Table('abilities', metadata,
         Column('id', Integer, primary_key=True),
         Column('name', String(100), unique=True))
 
-    pokemon_ability = Table('pokemon_ability', metadata,
+    pokemon_ability = Table('pokemon_abilities', metadata,
         Column('pokemon_id', Integer, ForeignKey('pokemon.id', ondelete='CASCADE')),
-        Column('ability_id', Integer, ForeignKey('ability.id', ondelete='CASCADE')))
+        Column('ability_id', Integer, ForeignKey('abilities.id', ondelete='CASCADE')))
 
-    move = Table('move', metadata,
+    move = Table('moves', metadata,
         Column('id', Integer, primary_key=True),
         Column('name', String(100), unique=True))
 
-    pokemon_move = Table('pokemon_move', metadata,
+    pokemon_move = Table('pokemon_moves', metadata,
         Column('pokemon_id', Integer, ForeignKey('pokemon.id', ondelete='CASCADE')),
-        Column('move_id', Integer, ForeignKey('move.id', ondelete='CASCADE')),
+        Column('move_id', Integer, ForeignKey('moves.id', ondelete='CASCADE')),
         Column('move_learn_method', String(100)))
 
     metadata.drop_all(engine)
     metadata.create_all(engine)
     print('All Tables Created')
 
+
 # Populate Pokemon Table
 def create_pokemon_table(df_pokemon, engine):
     df_pokemon.to_sql(name='pokemon', con=engine, if_exists='append', index=False)
     print('Pokemon Table Populated')
 
+
 # Populate Type Tables
 def create_type_tables(df_type, df_pokemon_type, engine):
-    df_type.to_sql(name='type', con=engine, if_exists='append', index=False)
+    df_type.to_sql(name='types', con=engine, if_exists='append', index=False)
     with engine.connect() as conn:
-        result = conn.execute(text('SELECT id, name FROM type;')).mappings()
+        result = conn.execute(text('SELECT id, name FROM types;')).mappings()
         id_map = {}
         for row in result:
             id_map[row['name']] = row['id']
         df_pokemon_type['type_id'] = df_pokemon_type['type_id'].map(id_map)
-    df_pokemon_type.to_sql(name='pokemon_type', con=engine, if_exists='append', index=False)
+    df_pokemon_type.to_sql(name='pokemon_types', con=engine, if_exists='append', index=False)
     print("Type and Junction Tables Populated")
+
 
 # Populate Abilities Tables
 def create_abilities_tables(df_ability, df_pokemon_ability, engine):
-    df_ability.to_sql(name='ability', con=engine, if_exists='append', index=False)
+    df_ability.to_sql(name='abilities', con=engine, if_exists='append', index=False)
     with engine.connect() as conn:
-        result = conn.execute(text('SELECT id, name FROM ability;')).mappings()
+        result = conn.execute(text('SELECT id, name FROM abilities;')).mappings()
         id_map = {}
         for row in result:
             id_map[row['name']] = row['id']
         df_pokemon_ability['ability_id'] = df_pokemon_ability['ability_id'].map(id_map)
-    df_pokemon_ability.to_sql(name='pokemon_ability', con=engine, if_exists='append', index=False)
+    df_pokemon_ability.to_sql(name='pokemon_abilities', con=engine, if_exists='append', index=False)
     print("Ability and Junction Tables Populated")
+
 
 # Populate Moves Tables
 def create_moves_tables(df_move, df_pokemon_move, engine):
-    df_move.to_sql(name='move', con=engine, if_exists='append', index=False)
+    df_move.to_sql(name='moves', con=engine, if_exists='append', index=False)
     with engine.connect() as conn:
-        result = conn.execute(text('SELECT id, name FROM move;')).mappings()
+        result = conn.execute(text('SELECT id, name FROM moves;')).mappings()
         id_map = {}
         for row in result:
             id_map[row['name']] = row['id']
-        df_pokemon_move['move_id'] = df_pokemon_move['move_id'].map(id_map)
-    df_pokemon_move.to_sql(name='pokemon_move', con=engine, if_exists='append', index=False)
+    df_pokemon_move['move_id'] = df_pokemon_move['move_id'].map(id_map)
+    df_pokemon_move.to_sql(name='pokemon_moves', con=engine, if_exists='append', index=False)
     print("Move and Junction Tables Populated")
+
 
 # Database Setup
 def setup_database_connection():
@@ -181,6 +186,7 @@ def setup_database_connection():
         create_database(engine.url)
     return engine
 
+
 # Query and Print
 def query_and_print_table(engine, table_name='pokemon'):
     with engine.connect() as conn:
@@ -188,14 +194,12 @@ def query_and_print_table(engine, table_name='pokemon'):
         for row in result:
             print(row)
 
+
 # Main
 def main():
-    configure_pandas()
 
     # Fetch raw data
     pokemon_raw_list = fetch_multiple_pokemon(10)
-
-    # pprint(pokemon_raw_list[0])  # For debugging
 
     # Clean for Pokemon table
     df_pokemon = clean_for_pokemon_table(pokemon_raw_list)
@@ -214,7 +218,7 @@ def main():
 
     # Setup Tables
     create_empty_tables(engine)
-    print(df_ability.head())
+
     # Save Tables
     create_pokemon_table(df_pokemon, engine)
     create_type_tables(df_type, df_pokemon_type, engine)
@@ -222,16 +226,27 @@ def main():
     create_moves_tables(df_move, df_pokemon_move, engine)
 
     # Query to verify
+    print('')
+    print('Sample of Pokemon Table Query:')
     query_and_print_table(engine, table_name='pokemon')
-    query_and_print_table(engine, table_name='type')
-    query_and_print_table(engine, table_name='pokemon_type')
-    query_and_print_table(engine, table_name='ability')
-    query_and_print_table(engine, table_name='pokemon_ability')
-    query_and_print_table(engine, table_name='move')
-    query_and_print_table(engine, table_name='pokemon_move')
+    print('')
+    print('Sample of Types Table Query:')
+    query_and_print_table(engine, table_name='types')
+    print('')
+    print('Sample of Pokemon Types Junction Table Query:')
+    query_and_print_table(engine, table_name='pokemon_types')
+    print('')
+    print('Sample of Abilities Table Query:')
+    query_and_print_table(engine, table_name='abilities')
+    print('')
+    print('Sample of Pokemon Abilities Junction Table Query:')
+    query_and_print_table(engine, table_name='pokemon_abilities')
+    print('')
+    print('Sample of Moves Table Query:')
+    query_and_print_table(engine, table_name='moves')
+    print('')
+    print('Sample of Pokemon Moves Table Query:')
+    query_and_print_table(engine, table_name='pokemon_moves')
 
 if __name__ == "__main__":
     main()
-
-
-# Add paulaked as collaborator
